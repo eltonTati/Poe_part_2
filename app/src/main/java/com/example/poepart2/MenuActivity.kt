2c406c57e683f545480a41dd84ec59f98742a797
stxphanx30 <ST10158209@vcconnect.edu.za>
2025-05-02 21:50:28 +0200

    home screen chnages

--- app/src/main/java/com/example/poepart2/MenuActivity.kt
+++ app/src/main/java/com/example/poepart2/MenuActivity.kt
@@ -1,5 +1,6 @@
 package com.example.poepart2
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
@@ -9,7 +10,7 @@ import androidx.drawerlayout.widget.DrawerLayout
 class MenuActivity : AppCompatActivity() {
 
     private lateinit var drawerLayout: DrawerLayout
-    private lateinit var settingsButton: TextView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +21,36 @@ class MenuActivity : AppCompatActivity() {
 
         menuIcon.setOnClickListener {
             drawerLayout.openDrawer(R.id.custom_nav_drawer2)
+        }
+        val acc= findViewById<TextView>(R.id.acc)
+        acc.setOnClickListener {
+            val intent = Intent(this, GuideActivity::class.java)
+            startActivity(intent)
+        }
+        val languageText = findViewById<TextView>(R.id.languageText)
+        languageText.setOnClickListener {
+            val intent = Intent(this, GuideActivity::class.java)
+            startActivity(intent)
+        }
+        val contactSupport = findViewById<TextView>(R.id.contactSupport)
+        contactSupport .setOnClickListener {
+            val intent = Intent(this, GuideActivity::class.java)
+            startActivity(intent)
+        }
+        val cat = findViewById<TextView>(R.id.cat)
+        cat.setOnClickListener {
+            val intent = Intent(this, GuideActivity::class.java)
+            startActivity(intent)
+        }
+        val curr = findViewById<TextView>(R.id.curr)
+        curr.setOnClickListener {
+            val intent = Intent(this, GuideActivity::class.java)
+            startActivity(intent)
+        }
+        val rank = findViewById<TextView>(R.id.rank)
+        rank.setOnClickListener {
+            val intent = Intent(this, GuideActivity::class.java)
+            startActivity(intent)
         }
     }
 }
