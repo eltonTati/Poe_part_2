c8733645f5e0fb2c7b658374b9fdc16784932e82
stxphanx30 <ST10158209@vcconnect.edu.za>
2025-06-08 04:36:09 +0200

    updates

--- app/src/main/java/com/example/poepart2/AnalyticsScreenActivity.kt
+++ app/src/main/java/com/example/poepart2/AnalyticsScreenActivity.kt
@@ -1,13 +1,30 @@
 package com.example.poepart2
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.room.Transaction
 
 
 class AnalyticsScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_analytics_screen)
+        findViewById<ImageView>(R.id.nav_home).setOnClickListener {
+            startActivity(Intent(this, MenuActivity::class.java))
         }
+        findViewById<ImageView>(R.id.navAnalysis).setOnClickListener {
+            startActivity(Intent(this, AnalyticsScreenActivity::class.java))
+        }
+        findViewById<ImageView>(R.id.navTransactions).setOnClickListener {
+            startActivity(Intent(this, WalletScreenActivity::class.java))
+        }
+        findViewById<ImageView>(R.id.nav_settings).setOnClickListener {
+            startActivity(Intent(this, UserScreenActivity::class.java))
+        }
+        }
+
     }
